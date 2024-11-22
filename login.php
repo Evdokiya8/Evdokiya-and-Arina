396121d2e679164e2056cd84744a03839aa726cf
Arina1810 <umetbaeva.arina.05@gmail.com>
2024-11-22 22:25:47 +0300

    login.php

--- login.php
+++ login.php
@@ -1,30 +1,31 @@
-<?php
-session_start();
-include 'config.php';
+<?php 
+session_start(); 
+include 'config.php'; 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    $username = $_POST['username']; 
+    $password = $_POST['password']; 
 
-    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $stmt->store_result();
-    
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($hashed_password);
-        $stmt->fetch();
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?"); 
+    $stmt->bind_param("s", $username); 
+    $stmt->execute(); 
+    $stmt->store_result(); 
+     
+    if ($stmt->num_rows > 0) { 
+        $stmt->bind_result($hashed_password); 
+        $stmt->fetch(); 
 
-        if (password_verify($password, $hashed_password)) {
-            $_SESSION['username'] = $username;
-            echo "Вход успешен!";
-        } else {
-            echo "Неверный пароль.";
-        }
-    } else {
-        echo "Пользователь не найден.";
-    }
+        if (password_verify($password, $hashed_password)) { 
+            $_SESSION['username'] = $username; 
+            header("Location: index.html"); 
+            exit();
+        } else { 
+            echo "Неверный пароль."; 
+        } 
+    } else { 
+        echo "Пользователь не найден."; 
+    } 
 
-    $stmt->close();
-}
+    $stmt->close(); 
+} 
 ?>
