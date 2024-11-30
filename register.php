9c3bc2513d7f2ae368433819c80d723b80efc5f4
Evdokiya8 <169051210+Evdokiya8@users.noreply.github.com>
2024-11-30 18:38:27 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,20 +1,29 @@
-<?php 
-include 'config.php'; 
+<?php
+require 'database.php'; 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
-    $username = $_POST['username']; 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
-
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)"); 
-    $stmt->bind_param("ss", $username, $password); 
-
-    if ($stmt->execute()) { 
-        header("Location: index.html");
+    $phone = $_POST['phone'];
+    $role = 'client'; 
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password, role, phone) VALUES (?, ?, ?, ?, ?);");
+    if( ! $stmt ){ //если ошибка - убиваем процесс и выводим сообщение об ошибке.
+    	die( "SQL Error: {$this->conn->errno} - {$this->conn->error}" );
+    }
+    $stmt->bind_param("sssss", $username, $email, $password, $role, $phone);
+    
+    if ($stmt->execute()) {
+        //echo "Регистрация успешна!";
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+            $_SESSION['role'] = $user['role'];
+            header("Location: /golovkina.e.p/my_project/html/main.html"); 
+       // header("Location: /golovkina.e.p/my_project/html/login.html");
+        
         exit();
-    } else { 
-        echo "Ошибка: " . $stmt->error; 
-    } 
-
-    $stmt->close(); 
-} 
+    } else {
+        echo "Ошибка: " . $stmt->error;
+    }
+}
 ?>
